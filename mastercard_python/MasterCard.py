5f25e87a7644cd7d4ef3fb56cddefd757e804971
Samuel Marks <samuelmarks@gmail.com>
2015-01-30 17:29:51 +1100

    Now my MasterCard.py also returns the 500 (as stolen.py does).

--- mastercard_python/MasterCard.py
+++ mastercard_python/MasterCard.py
@@ -1,20 +1,40 @@
 #!/usr/bin/env python
 
-from __init__ import BASE_API_URI
+from os import environ
 
 from requests import get as http_get
+from requests_oauthlib import OAuth1
+from requests_oauthlib.oauth1_session import SIGNATURE_RSA
+
+from __init__ import BASE_API_URI
+from stolen import get_private_key_from_jks
+
 from pprint import PrettyPrinter
 
 pp = PrettyPrinter(indent=4).pprint
 
 
-class MasterCard(object):
-    def _request(self, service, item, _id):
+def master_card(auth):
+    def request(service, item, _id):
         url = BASE_API_URI.format(service=service, item=item, id=_id)
         print 'GET:', url
-        return (lambda r: r.text if r.status_code == 200 else {'status_code': r.status_code})(http_get(url))
+        return (lambda r: r.text if r.status_code == 200 or r.text == '<HTML>OK</HTML>' else {
+            'status_code': r.status_code, 'text': r.text})(http_get(url, auth=request.auth))
+
+    request.auth = auth
+
+    return request
 
 
 if __name__ == '__main__':
-    mc = MasterCard()
-    print pp(mc._request('restaurants', 'restaurant', 6155332))
+    client_key = environ['MC_CLIENT_ID']
+
+    private_key = environ['MC_PRIVATE_KEY']
+    certificate = environ.get('MC_CERTIFICATE')
+    alias = 'mc'  # That's my alias
+
+    if private_key.endswith('.jks'):
+        private_key = get_private_key_from_jks(jks=private_key, alias=alias, key_store_password='')
+    mc_oauth = OAuth1(client_key=client_key, signature_method=SIGNATURE_RSA, rsa_key=private_key)
+    mc = master_card(mc_oauth)
+    print pp(mc('restaurants', 'restaurant', 6155332))
